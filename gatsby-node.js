7f069bb5739a6bd4fd158a48d81e0d53213af126
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2021-12-06 12:26:32 +0200

    second commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,88 @@
-exports.createPages = async ({ actions }) => {
+
+
+
+
+/*export async function createPages({ actions }) {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+}
+*/
+
+
+exports.createPages = async ({boundActionCreators, graphql}) {
+  const {createPage} = boundActionCreators
+
+  const postTemplate = resolve(`./src/templates/blog-posts.js`);
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+              date
+              author
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if(res.errors) {
+      return Promise.reject(res.errors)
+    }
+
+    res.data.allMarkdownRemark.edges.forEach(({node}) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate
+      })
+    })
+  })
+}
+
+
+export function createPages({ boundActionCreators, graphql }) {
+  const { createPage } = boundActionCreators
+
+  const postTemplate = resolve(`./src/templates/blog-posts.js`)
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+              date
+              author
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) {
+      return Promise.reject(res.errors)
+    }
+
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+      })
+    })
   })
 }
